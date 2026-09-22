bbfe55025825df2434d17ac0672bed0ecfbf0cb9
PTarmale <prachitarmale90@gmail.com>
2026-09-22 07:55:43 +0530

    Refactor data generators to use DATASET_DIR
    Updated data generator to use a single dataset directory and added validation split.

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,42 +1,45 @@
+import os
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 IMG_SIZE = 128
 BATCH_SIZE = 32
 
-TRAIN_PATH = "dataset/NEU-DET/train/images"
-VALIDATION_PATH = "dataset/NEU-DET/validation/images"
+DATASET_DIR = "../dataset"
 
 
 def create_data_generators():
 
     train_datagen = ImageDataGenerator(
-        rescale=1.0 / 255,
+        rescale=1.0 / 255.0,
         rotation_range=10,
         width_shift_range=0.1,
         height_shift_range=0.1,
-        zoom_range=0.1,
-        horizontal_flip=True
+        horizontal_flip=True,
+        validation_split=0.2
     )
 
     validation_datagen = ImageDataGenerator(
-        rescale=1.0 / 255
+        rescale=1.0 / 255.0,
+        validation_split=0.2
     )
 
     train_data = train_datagen.flow_from_directory(
-        TRAIN_PATH,
+        DATASET_DIR,
         target_size=(IMG_SIZE, IMG_SIZE),
         color_mode="grayscale",
         batch_size=BATCH_SIZE,
         class_mode="categorical",
+        subset="training",
         shuffle=True
     )
 
     validation_data = validation_datagen.flow_from_directory(
-        VALIDATION_PATH,
+        DATASET_DIR,
         target_size=(IMG_SIZE, IMG_SIZE),
         color_mode="grayscale",
         batch_size=BATCH_SIZE,
         class_mode="categorical",
+        subset="validation",
         shuffle=False
     )
 
